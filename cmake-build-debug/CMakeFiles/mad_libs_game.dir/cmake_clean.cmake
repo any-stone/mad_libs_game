98750e19901db586960a8f6ea99d4f0af7c0ec80
any-stone <kuxa.business@gmail.com>
2021-08-10 19:01:50 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/mad_libs_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mad_libs_game.dir/main.cpp.o"
+  "mad_libs_game"
+  "mad_libs_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mad_libs_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
